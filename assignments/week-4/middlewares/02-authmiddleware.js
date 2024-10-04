a78a8b9f6f176f5f78dbda16c3198d832fd5fa90
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-10-05 02:00:07 +0530

    updated project

--- assignments/week-4/middlewares/02-authmiddleware.js
+++ assignments/week-4/middlewares/02-authmiddleware.js
@@ -8,7 +8,11 @@ const VALID_API_KEY = '100xdevs_cohort3_super_secret_valid_api_key'; // key is 1
 
 // Middleware to check for a valid API key
 function authenticateAPIKey(req, res, next) {
-    //  authenticate APIKey here
+    let key = req.headers['100xdevs-api-key']
+    if (key === VALID_API_KEY) {
+        next()
+    }
+    res.status(401).json({ message: 'Invalid or missing API key' });
 }
 
 app.use(authenticateAPIKey);
--- assignments/week-5/backend/index.js
+++ assignments/week-5/backend/index.js
@@ -1,1 +1,16 @@
-// start writing from here
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(express.json());
+
+app.get("/", (req, res)=> res.send("I am Healthy"));
+
+app.get("/",)
+
+app.listen(port, ()=> console.log(`server is running at http://localhost:${port}`));
